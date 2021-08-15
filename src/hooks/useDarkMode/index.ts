18216380c7fb50c9fa9514a3760723f1ff76e39a
Cobelt <paul_emile@hotmail.fr>
2021-08-16 01:49:06 +0200

    feat(gridify)+fix(exports): export fixed + updated useGridify to something that looks like more a hook

--- src/hooks/useDarkMode/index.ts
+++ src/hooks/useDarkMode/index.ts
@@ -1,5 +1,5 @@
 import { useContext } from 'react'
 import { DarkModeContext } from '../../contexts/darkMode'
 
-const useDarkMode = useContext(DarkModeContext)
+const useDarkMode = () => useContext(DarkModeContext)
 export default useDarkMode
--- src/hooks/useGridify/index.ts
+++ src/hooks/useGridify/index.ts
@@ -1,3 +1,4 @@
+import { useState, useEffect } from 'react'
 // types
 import type { CSSProperties } from 'react'
 import type { ResponsiveOrValue } from '../../types'
@@ -20,21 +21,20 @@ export interface GridifyProps {
     justify?: string
     align?: string
 
-    show?: boolean
-    hide?: boolean
-
     shouldTransmitProps?: boolean
     componentName?: string
 }
 
-export interface ReturnProps {
-    className: string
-    hide: boolean
-    style: CSSProperties
+export interface TransmissibleProps {
     col?: ResponsiveOrValue
     row?: ResponsiveOrValue
     width?: ResponsiveOrValue
     height?: ResponsiveOrValue
+}
+
+export interface ReturnProps extends TransmissibleProps {
+    className: string
+    style: CSSProperties
 }
 
 function useGridify({
@@ -45,76 +45,87 @@ function useGridify({
     fullWidth = false,
     fullHeight = false,
 
-    className,
-    style,
+    className: currentClassName,
+    style: currentStyle,
     position,
     justify,
     align,
 
-    show,
-    hide,
-
     shouldTransmitProps,
     componentName,
 }: GridifyProps): ReturnProps {
+    const [className, setClassName] = useState<string>(currentClassName ?? '')
+    const [style, setStyle] = useState<CSSProperties>(currentStyle ?? {})
+
+    const [props, setProps] = useState<TransmissibleProps>({})
+
     const col = position === 'fixed' ? 0 : defaultCol
     const row = position === 'fixed' ? 0 : defaultRow
 
-    const type = componentName || 'GridifiedElement'
+    useEffect(() => {
+        setClassName(
+            [
+                camelToKebab(componentName ?? 'GridifiedElement'),
+                currentClassName,
+                justify && `justify-${justify}`,
+                align && `align-${align}`,
+                position,
+            ]
+                .filter(Boolean)
+                .join(' '),
+        )
+    }, [currentClassName, componentName, justify, align, position])
 
-    // css variables
-    const styles = {}
-    if (
-        !(width instanceof Object) &&
-        parseInt(`${width}`, 10) <= 0 &&
-        !(height instanceof Object) &&
-        parseInt(`${height}`, 10) <= 0
-    ) {
-        styles['display'] = 'none'
-    } else {
-        ['xs', 'sm', 'md', 'lg', 'xl', 'xxl', 'xxxl'].forEach(size => {
-            const suffix = size !== 'xs' ? `-${size}` : ''
+    useEffect(() => {
+        // css variables
+        const styles = {}
+        if (
+            !(width instanceof Object) &&
+            parseInt(`${width}`, 10) <= 0 &&
+            !(height instanceof Object) &&
+            parseInt(`${height}`, 10) <= 0
+        ) {
+            styles['display'] = 'none'
+        } else {
+            ;['xs', 'sm', 'md', 'lg', 'xl', 'xxl', 'xxxl'].forEach(size => {
+                const suffix = size !== 'xs' ? `-${size}` : ''
 
-            styles[`--col${suffix}`] = col && getCSSVarForDimension({
-                dimension: col,
-                size,
-                defaultValue: 'auto',
+                styles[`--col${suffix}`] =
+                    col &&
+                    getCSSVarForDimension({
+                        dimension: col,
+                        size,
+                        defaultValue: 'auto',
+                    })
+                styles[`--row${suffix}`] =
+                    row &&
+                    getCSSVarForDimension({
+                        dimension: row,
+                        size,
+                        defaultValue: 'auto',
+                    })
+                styles[`--width${suffix}`] = getCSSVarForDimension({
+                    dimension: fullWidth ? -1 : width,
+                    size,
+                    defaultValue: 1,
+                    getPrefix: wdth => wdth >= 0 && 'span ',
+                })
+                styles[`--height${suffix}`] = getCSSVarForDimension({
+                    dimension: fullHeight ? -1 : height,
+                    size,
+                    defaultValue: 1,
+                    getPrefix: hght => hght >= 0 && 'span ',
+                })
             })
-            styles[`--row${suffix}`] = row && getCSSVarForDimension({
-                dimension: row,
-                size,
-                defaultValue: 'auto',
-            })
-            styles[`--width${suffix}`] = getCSSVarForDimension({
-                dimension: fullWidth ? -1 : width,
-                size,
-                defaultValue: 1,
-                getPrefix: wdth => wdth >= 0 && 'span ',
-            })
-            styles[`--height${suffix}`] = getCSSVarForDimension({
-                dimension: fullHeight ? -1 : height,
-                size,
-                defaultValue: 1,
-                getPrefix: hght => hght >= 0 && 'span ',
-            })
-        })
-    }
+        }
+        setStyle({ ...styles, ...currentStyle })
+    }, [col, row, width, height, fullWidth, fullHeight, currentStyle, componentName])
 
-    return {
-        hide: !(show ?? true) || (hide ?? false),
+    useEffect(() => {
+        setProps(shouldTransmitProps ? { col, row, width, height } : {})
+    }, [shouldTransmitProps, col, row, width, height])
 
-        className: [
-            camelToKebab(type),
-            className,
-            justify && `justify-${justify}`,
-            align && `align-${align}`,
-            position,
-        ]
-            .filter(Boolean)
-            .join(' '),
-        style: { ...styles, ...style },
-        ...(shouldTransmitProps ? { col, row, width, height } : {}),
-    }
+    return { className, style, ...props }
 }
 
 export default useGridify
